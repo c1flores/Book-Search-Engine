71b0efd8c28b64638eb8fefc0f845366460bb7b9
Christian Flores <c1flores@ucsd.edu>
2023-01-17 19:39:54 -0800

    Finish utilities' logic and configure main app.js for proper use of routes, components, apollo server, and JWT authentication

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,19 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
