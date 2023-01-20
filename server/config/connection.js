76960a5738d10214f8f817507e9a8979378b779b
Christian Flores <c1flores@ucsd.edu>
2023-01-20 14:39:28 -0800

    Final configuration for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test' , {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks' , {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
