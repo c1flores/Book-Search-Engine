f3b7752fa97428d6276a3c88b7e93d917809bf68
Christian Flores <c1flores@ucsd.edu>
2023-01-20 11:22:20 -0800

    Configuring connection file for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test' , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 module.exports = mongoose.connection;
