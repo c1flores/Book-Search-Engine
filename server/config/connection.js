77797065ea949f5f9e9e7200eb3e6b4b6ae1ff5c
Christian Flores <c1flores@ucsd.edu>
2023-01-20 11:34:59 -0800

    Confgiuring mongodb for deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test' , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useFindAndModify: true
 });
 
 module.exports = mongoose.connection;
